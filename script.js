07be2da3e76832023299017f898f3eebb8ce1c84
PabloCruz <pablocruzmiguez016@gmail.com>
2024-07-10 00:42:08 -0300

    Curso 6: Fin aula 3 y comienzo de aula 4

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const url = "https://apod.nasa.gov/apod/image/0604/M81_M82_schedler_c25.jpg";
+
+//buscar que es fetch en js
+//estamos haciendo un GET
+//fetch devuelve una promise / promesa
+fetch(url);
+
+//.then cuando la promesa se cumple "fullfield(?"
+//.catch cuando la promesa es rechazada "rejected"
+
